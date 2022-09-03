3af47e188da8368adf92d744356c19ac4f54127d
KashanAdnan <khanzaidaboy@gmail.com>
2022-09-03 09:34:50 -0700

    saturday-class-github(3-9-2022)

---
+++ js/questions.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Which Elem Use To Bold The Text?",
+    answer: "b Tags Use To",
+    options: [
+      "br Tags Use To",
+      "b Tags Use To",
+      "table Tags Use To",
+      "hr Tags Use To"
+    ]
+  },
+    {
+    numb: 7,
+    question: "Which Elem Use To Break The Line?",
+    answer: "br Tags Use To",
+    options:[
+      "b Tags Use To",
+      "form Tags Use To",
+      "br Tags Use To",
+      "mark Tags Use To"
+    ]
+  },
+    {
+    numb: 8,
+    question: "Which Element Help to Make Form?",
+    answer: "Form Tags Use To",
+    options: [
+      "Form Tags Use To",
+      "Table Tags Use To",
+      "Head Tags Use To",
+      "img Tag Use To"
+    ]
+  },
+    {
+    numb: 9,
+    question: "How Many Tags Are in HTML?",
+    answer: "110 Tags",
+    options: [
+      "112 Tags",
+      "110 Tags",
+      "34 Tags",
+      "181 Tags"
+    ]
+  },
+    {
+    numb: 10,
+    question: "Who Are You?",
+    answer: "Human",
+    options: [
+      "Human",
+      "Parrot",
+      "Bird",
+      "Animal"
+    ]
+  }
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
